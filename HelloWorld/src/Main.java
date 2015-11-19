3cd27ece9e868939cc5b04544f4bcae127c10879
trovgiov <trovgiov@gmail.com>
2015-11-19 10:37:07 +0100

    4

--- HelloWorld/src/Main.java
+++ HelloWorld/src/Main.java
@@ -4,7 +4,7 @@ public class Main {
 	
 	public static void main (String args[]){
 		
-		System.out.println("Ciao");
-		
+		System.out.println("Ciao 2.0");
+		System.out.println("Ciao da git");
 	}
 }
